dedc3aa76f899ef9834c247b27120035ad92862c
Pixelated-Raptor <tomczyk.fabianmarkus@fh-swf.de>
2023-06-06 23:23:28 +0200

    Added Subplots

--- plots/subplots.py
+++ plots/subplots.py
@@ -3,20 +3,33 @@ import sqlite3, pandas, matplotlib.pyplot as plt
 con = sqlite3.connect("../solar_data.db")
 
 sql = """select
-            acpower
+            acpower, yieldtoday, consumeenergy, batPower
         from
             DOWNLOADED_SOLAX_INFO
-        order by
-            time_of_request desc
-        limit 50;
+        where
+            date(time_of_request, 'localtime') = current_date;
         """
 
 data = pandas.read_sql(sql, con)
 
+# AC Power
+plt.subplot(2, 2, 1)
 plt.plot(data.acpower, label = "AC Power")
-plt.grid()
-plt.legend()
 plt.title("AC Power")
-#plt.show()
-#plt.savefig('/var/www/html/proto/cheese.png')
-plt.savefig('cheese2.png')
+
+# Yield Today
+plt.subplot(2, 2, 2)
+plt.plot(data.yieldtoday, label = "Yield Today")
+plt.title("Yield Today")
+
+# Consume Energy
+plt.subplot(2, 2, 3)
+plt.plot(data.consumeenergy, label = "Consume Energy")
+plt.title("Consume Energy")
+
+# Battery Power
+plt.subplot(2, 2, 4)
+plt.plot(data.batPower, label = "Battery Power")
+plt.title("Battery Power")
+
+plt.savefig('sub0.png', dpi=400)
