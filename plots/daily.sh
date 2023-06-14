f5c018c52fcfb0f1bfb0aad6178de7bae5adf3e8
Pixelated-Raptor <tomczyk.fabianmarkus@fh-swf.de>
2023-06-15 00:05:44 +0200

    made savepath absolute

--- plots/daily.sh
+++ plots/daily.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 /usr/bin/python3 /home/kiwi/project/plots/dailyplot.py >> /home/kiwi/logs/plots_log.txt 2>&1
 
 cp /home/kiwi/project/plots/daily0.png /var/www/html/proto/graphs/daily0.png
