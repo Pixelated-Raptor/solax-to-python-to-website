b4b8cad1e76452fd6e512b840f7e0a0b48c88440
Pixelated-Raptor <tomczyk.fabianmarkus@fh-swf.de>
2023-06-19 12:08:47 +0200

    7 Tage Übersicht

--- plots/dailyplot.py
+++ plots/dailyplot.py
@@ -19,28 +19,34 @@ data = pandas.read_sql(sql, con)
 # AC Power
 plt.subplot(2, 2, 1)
 plt.plot(data.ac, label = "AC Power")
-plt.title("AC Power [W]")
+plt.title("AC Leistung [W]")
+plt.xlabel("Uhrzeit")
 plt.grid(linestyle = '--', linewidth = 0.5)
 
 # Yield Today
 plt.subplot(2, 2, 2)
 plt.plot(data.yieldtoday, label = "Yield Today")
-plt.title("Yield Today [kWh]")
+plt.title("Tagesertrag [kWh]")
+plt.xlabel("Uhrzeit")
 plt.grid(linestyle = '--', linewidth = 0.5)
 
 # Consume Energy
 plt.subplot(2, 2, 3)
 plt.plot(data.consume, label = "Consume Energy")
-plt.title("Consume Energy [kWh]")
+plt.title("Eigennutzung [kWh]")
+plt.xlabel("Uhrzeit")
 plt.grid(linestyle = '--', linewidth = 0.5)
 
 # Battery Power
 plt.subplot(2, 2, 4)
 plt.plot(data.batPower, label = "Battery Power")
-plt.title("Battery Power [W]")
+plt.title("Batterieleistung [W]")
+plt.xlabel("Uhrzeit")
 plt.grid(linestyle = '--', linewidth = 0.5)
 
+x_tick_labels = ['0 uhr', '1 Uhr', '2 Uhr', '3 Uhr', '4 Uhr', '5 Uhr', '6 Uhr', '7 Uhr', '8 Uhr', '9 Uhr', '10 Uhr', '11 Uhr', '12 Uhr', '13 Uhr', '14 Uhr', '15 Uhr', '16 Uhr', '17 Uhr', '18 Uhr', '19 Uhr', '20 Uhr', '21 Uhr', '22 Uhr', '23 Uhr']
 plt.tight_layout()
-plt.suptitle("Today's Data")
+#plt.set_xticklabels(x_tick_labels)
+plt.suptitle("Heutige Daten")
 plt.subplots_adjust(top=0.85)
 plt.savefig('/home/kiwi/project/plots/daily0.png', dpi=150)
